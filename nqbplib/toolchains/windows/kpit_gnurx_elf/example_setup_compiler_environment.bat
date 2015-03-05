a050ca741057bc52dc81c5e3368291e371800577
John Taylor <john.t.taylor@gmail.com>
2015-03-05 01:02:57 -0600

    add example set-environment script for the kpit toolchain

---
+++ nqbplib/toolchains/windows/kpit_gnurx_elf/example_setup_compiler_environment.bat
@@ -0,0 +1,5 @@
+@echo off
+set KPIT_GNU_RX_ROOT=c:\progra~2\renesas\hew\tools\kpitcu~1\gnurx-elf\v11.03\rx-elf
+set PATH=%KPIT_GNU_RX_ROOT%\bin;%KPIT_GNU_RX_ROOT%\rx-elf\bin;%PATH%
+::set PATH=%KPIT_GNU_RX_ROOT%\rx-elf\bin;%PATH%
+echo:KPit Cummins Gnu-RX (Elf) Environment setup
